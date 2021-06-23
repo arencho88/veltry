7a3a35679db4686405d2f45581643c479d5d3636
Aren <california0107@mail.ru>
2021-06-23 19:13:52 +0300

    сделал полностью адаптив

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-$(function(){
+let nav = document.querySelector('.header__nav')
+let burgerBtn = document.querySelector('.header__nav-burger');
 
-});
+burgerBtn.onclick = navToggle;
+
+function navToggle() {
+	if (nav.classList.contains('header__nav--closed')) {
+		nav.classList.remove('header__nav--closed');
+		nav.classList.add('header__nav--opened');
+	} else {
+		nav.classList.remove('header__nav--opened');
+		nav.classList.add('header__nav--closed');
+	}
+}
+
